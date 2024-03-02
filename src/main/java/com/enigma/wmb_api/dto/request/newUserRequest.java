83807862b9d42b06c2de8b08f89ddccc17ff5182
Irfan Yahya Abdillah <irfanyahyaa@gmail.com>
2024-03-03 06:41:19 +0700

    [FEATURE] add validation input with regex for createNewUser method

--- src/main/java/com/enigma/wmb_api/dto/request/newUserRequest.java
+++ src/main/java/com/enigma/wmb_api/dto/request/newUserRequest.java
@@ -1,6 +1,7 @@
 package com.enigma.wmb_api.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -10,8 +11,10 @@ import lombok.*;
 @Builder
 public class newUserRequest {
     @NotBlank(message = "name cannot be blank")
+    @Pattern(regexp = "[a-zA-Z]+", message = "name must be alphabetic")
     private String name;
-    @NotBlank(message = "mobile_phone_no cannot be blank")
+    @NotBlank(message = "mobilePhoneNo cannot be blank")
+    @Pattern(regexp = "[0-9]+", message = "mobilePhoneNo must be numeric")
     private String mobilePhoneNo;
     private Boolean isMember;
 }
